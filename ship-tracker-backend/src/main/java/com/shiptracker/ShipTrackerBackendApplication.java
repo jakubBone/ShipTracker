7f7067cd84863ce9883feaa6f810d73008327072
jakubBone <jakub.bone1990@gmail.com>
2026-02-10 14:38:07 +0100

    chore(backend): add Spring backend

---
+++ ship-tracker-backend/src/main/java/com/shiptracker/ShipTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shiptracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ ship-tracker-backend/src/test/java/com/shiptracker/ShipTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiptracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
